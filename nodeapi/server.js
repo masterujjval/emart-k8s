5881e327a23844553b30a34dab3f98414b5d705f
Ujjwal gupta <guptarickey3@gmail.com>
2025-10-15 15:52:54 +0530

    Update server.js

--- nodeapi/server.js
+++ nodeapi/server.js
@@ -13,6 +13,21 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
+
+// here iam adding this for metrics
+const client = require('prom-client');
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests received',
+});
+register.registerMetric(httpRequestCounter);
+
+
+// till here 
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,14 +45,36 @@ app.get('/', (req,res) => {
 app.use("/api/user", userRoutes);
 app.use("/api/shop", shopRoutes);
 
+// here we go again
+app.use((req, res, next) => {
+  httpRequestCounter.inc();
+  next();
+});
+
+// here we stop 
+
+
+// here we go again
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+// here we stop 
 
 mongoose
   .connect(mongooseURI)
   .then(() => {
-    const port = process.env.PORT || 5000;
-    const server = app.listen(port, () => {
-      console.log("Server running on port".magenta, colors.yellow(port));
-    });
+  
     console.log("\nConnected to".magenta, "E-MART".cyan, "database".magenta);
   })
   .catch(err => console.log("Error connecting to database".cyan, err));
+
+
+// here we go again
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// here we stop again
